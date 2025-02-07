598f1a049f7aa6f7b25614cedaaa951ed26a8604
AnnaZhouJJ <annauiuxdesign@gmail.com>
2025-02-07 21:31:15 +1100

    Default bg color changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div className="flex flex-col items-center bg-red-100 m-96">
       <h1>Hello I'm Anna Zhou.</h1>
       <h2>I'm a ui/ux designer!</h2>
-      {/* <Link href={"/about"}>Click here to go to About</Link> */}
     </div>
   );
 }
