df34c51439277abc854bab9de41ff666b2f023b5
AnnaZhouJJ <annauiuxdesign@gmail.com>
2025-02-10 22:38:57 +1100

    Nav bar: client pathname added

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavBar() {
+    const pathname = usePathname();
     return (
         <div className="h-[80px] w-screen sticky bg-white shadow-[0_4px_40px_0_rgba(212,212,212,0.25)] px-12
                         flex items-center justify-between">
@@ -10,9 +14,22 @@ export function NavBar() {
             </div>
             
             <div className="flex gap-[35px] font-normal text-lg text-gray-800">
-                <Link href={"/"}>Portfolio</Link>
-                <Link href={"/about"}>About Me</Link>
-                <Link href={"/contact"}>Contact</Link>
+                <Link href={"/"} 
+                    className={`hover:text-[#F2709C] ${pathname === "/" ? "text-[#F2709C]" : ""}`}>
+                    Portfolio
+                </Link>
+
+                <Link href={"/about"} 
+                    className={`hover:text-[#F2709C] ${pathname === "/about" ? "text-[#F2709C]" : ""}`}
+                >
+                    About Me
+                </Link>
+
+                <Link href={"/contact"} 
+                    className={`hover:text-[#F2709C] ${pathname === "/contact" ? "text-[#F2709C]" : ""}`}
+                >
+                    Contact
+                </Link>
             </div>
         
         </div>
